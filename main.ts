82d0d087da20401b9201b0c02908ecc64fa8157a
dcerver <121509667+dcerver@users.noreply.github.com>
2023-04-27 09:52:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,10 +12,12 @@ input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
     basic.showIcon(IconNames.Yes)
     basic.pause(200)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(0)
     basic.showIcon(IconNames.Yes)
     basic.pause(200)
+    basic.clearScreen()
 })
 radio.setGroup(76)
