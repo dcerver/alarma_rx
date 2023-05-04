b6a31df7e836bc929b211b600a3f59762b06c93e
dcerver <121509667+dcerver@users.noreply.github.com>
2023-05-04 09:41:47 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,5 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        basic.showIcon(IconNames.Happy)
-    } else if (receivedNumber == 1) {
-        basic.showIcon(IconNames.Sad)
-        basic.pause(200)
-        basic.clearScreen()
-        basic.pause(200)
-    }
+    Alarma = receivedNumber
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
@@ -22,4 +15,16 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(200)
     basic.clearScreen()
 })
+let Alarma = 0
 radio.setGroup(76)
+Alarma = 0
+basic.forever(function () {
+    if (Alarma == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else if (Alarma == 1) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
